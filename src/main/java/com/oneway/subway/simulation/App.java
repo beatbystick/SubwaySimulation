2af6bd4808f6323cc222cd2ab2d92bace1d17bfc
Beatbystick <beatbystick@gmail.com>
2015-04-21 20:07:13 -0400

    Init

---
+++ src/main/java/com/oneway/subway/simulation/App.java
@@ -0,0 +1,13 @@
+package com.oneway.subway.simulation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
